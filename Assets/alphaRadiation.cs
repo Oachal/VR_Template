47e10fb40b48df220f57c846a655588922e4d70f
Christopher Oakley <coakley@spelman.edu>
2023-03-06 13:36:12 -0500

    added C#
    non-working alpha radiation code.

---
+++ Assets/alphaRadiation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alphaRadiation : MonoBehaviour
+{
+    public int protonNumber = 92; // The starting number of protons (uranium-238)
+    public int neutronNumber = 146; // The starting number of neutrons (uranium-238)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (WAND BUTTON == "AlphaParticle")
+        {
+            // An alpha particle has collided with this game object
+            // Decrease the proton and neutron numbers by 2
+            protonNumber -= 2;
+            neutronNumber -= 2;
+
+            Debug.Log("An alpha particle has collided with the nucleus!");
+            Debug.Log("New proton number: " + protonNumber);
+            Debug.Log("New neutron number: " + neutronNumber);
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
